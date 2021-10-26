068813771e26eb1013c3e79f81907ef4b39ff680
Sriraj <behera.sriraj2911@gmail.com>
2021-10-26 23:43:35 +0530

    fixed tabs enrollment and exported AlbumType data

--- types.tsx
+++ types.tsx
@@ -9,7 +9,7 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
+    interface RootParamList extends RootStackParamList { }
   }
 }
 
@@ -27,9 +27,17 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 export type RootTabParamList = {
   TabOne: undefined;
   TabTwo: undefined;
+  TabThree: undefined;
+  TabFour: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
   BottomTabScreenProps<RootTabParamList, Screen>,
   NativeStackScreenProps<RootStackParamList>
 >;
+
+export type AlbumType = {
+  id: string;
+  imageUri: string;
+  artistsHeadline: string;
+}
