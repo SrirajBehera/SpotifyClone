c4ff60ee4eb3a7615eebdfe2af2309047ab9db3e
Sriraj <behera.sriraj2911@gmail.com>
2021-10-26 23:40:47 +0530

    made album categories component

---
+++ components/AlbumCategory/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, View, FlatList } from "react-native";
+import { AlbumType } from "../../types";
+import Album from "../Album";
+import styles from "./styles";
+
+export type AlbumCategoryProps = {
+	title: string;
+	albums: [AlbumType];
+}
+
+const AlbumCategory = (props: AlbumCategoryProps) => {
+	return (
+		<View style={styles.container}>
+			{/* Title of the albumCategory */}
+			<Text style={styles.title}>{props.title}</Text>
+			{/* List of albums */}
+			<FlatList 
+				data={props.albums}
+				renderItem={({item}) => <Album album={item}/>}
+				keyExtractor={(item) => item.id}
+				horizontal
+			/>
+		</View>
+	)
+}
+
+export default AlbumCategory;
